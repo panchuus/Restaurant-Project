1555ca35e0165b99cd5a9511973ebe9e113731eb
panchuus <panchamikamath1999@gmail.com>
2020-11-11 19:21:10 +0530

    restaurant

---
+++ build/precache-manifest.ed422909c1718a9caf63a277494dac1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9585291c81ad786fa40b76f3324b0c97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86c44980b8a09e0b6938",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "fe5f9262ea1ea5f7ced1",
+    "url": "/static/css/main.36b83c6e.chunk.css"
+  },
+  {
+    "revision": "86c44980b8a09e0b6938",
+    "url": "/static/js/2.6296b966.chunk.js"
+  },
+  {
+    "revision": "60340bbd69ba9570a0ce069546961ca3",
+    "url": "/static/js/2.6296b966.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe5f9262ea1ea5f7ced1",
+    "url": "/static/js/main.017d485b.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
